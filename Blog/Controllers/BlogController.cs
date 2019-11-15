8b8bc2896b958ceb5f0437332f60eba699d03fd8
ImanberdiA <31826437+ImanberdiA@users.noreply.github.com>
2019-11-15 22:01:52 +0600

    Unfinished edit post function

--- Blog/Controllers/BlogController.cs
+++ Blog/Controllers/BlogController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Web;
@@ -16,20 +17,23 @@ namespace Blog.Controllers
         AppIdentityDbContext db = new AppIdentityDbContext();
 
         // GET: Blog
-        public ActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            IEnumerable<AppUser> users = UserManager.Users.ToList();
-            return View(users);
+            //IEnumerable<AppUser> users = UserManager.Users.ToList();
+            //return View(users);
+
+            AppUser user = await UserManager.FindByNameAsync(HttpContext.User.Identity.Name);
+            return View(user);
         }
 
         #region Создание поста
-        public ActionResult CreatePost()
+        public ActionResult Create()
         {
             return View();
         }
 
         [HttpPost]
-        public async Task<ActionResult> CreatePost(Post post)
+        public async Task<ActionResult> Create(Post post)
         {
             AppUser cur_user = await UserManager.FindByNameAsync(HttpContext.User.Identity.Name);
             cur_user.Posts.Add(post);
@@ -40,7 +44,7 @@ namespace Blog.Controllers
         #endregion
 
         #region Удаление поста
-        public async Task<ActionResult> DeletePost(string id)
+        public async Task<ActionResult> Delete(string id)
         {
             Post post = db.Posts.FirstOrDefault(p => p.Id.ToString() == id);
 
@@ -50,6 +54,38 @@ namespace Blog.Controllers
                 await db.SaveChangesAsync();
             }
 
+            return RedirectToAction("Index");
+        }
+        #endregion
+
+        #region Редактирование поста
+        public ActionResult Edit(string id)
+        {
+            Post post;
+            if (id != null)
+            {
+                post = db.Posts.FirstOrDefault(p => p.Id.ToString() == id);
+            }
+            else
+            {
+                return HttpNotFound();
+            }            
+
+            if (post != null)
+            {
+                return View(post);
+            }
+            else
+            {
+                return new HttpStatusCodeResult(404);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Edit(Post post)
+        {
+            db.Entry(post).State = EntityState.Modified;
+            await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
         #endregion
