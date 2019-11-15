45d24272eff1f396b2dd6cadfa352397c32fd2de
ImanberdiA <31826437+ImanberdiA@users.noreply.github.com>
2019-11-16 00:15:27 +0600

    Edit post function was added

--- Blog/Models/Post.cs
+++ Blog/Models/Post.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace Blog.Models
 {
     public class Post
     {
+        [HiddenInput(DisplayValue=false)]
         public int Id { get; set; }
 
         public string Title { get; set; }
